1faec6e1439801f32340677b6c3d8db8987a8ed3
mkvsshine <hongbiao.yang@gmail.com>
2014-10-01 13:16:18 -0400

    Change animation effect
    change animation effect, add one more frame

--- src/com/utkise/TTSProj2/CONSTANT.java
+++ src/com/utkise/TTSProj2/CONSTANT.java
@@ -4,7 +4,7 @@ package com.utkise.TTSProj2;
  * Created by Bill on 9/15/14.
  */
 public class CONSTANT {
-    public final static int START = 1;
+    public final static int START = 0;
     public final static int MIDDLE = 1;
     public final static int END = 2;
 
